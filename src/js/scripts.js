16eb2e203adb844bce1827404d8db9c61cdc7e05
Rafael <rafdigital22@gmail.com>
2022-09-09 22:37:01 +0300

    responsive for section-carousel

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   $(".carousel__slides").slick({
     speed: 1100,
-    // adaptiveHeight: true,
+    adaptiveHeight: true,
+    autoplay: true,
+
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="icons/chevron-left.png"/></button>',
+      '<button type="button" class="slick-prev slick-arrow"><img src="icons/chevron-left.png"/></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="icons/chevron-right.png"/></button>',
+      '<button type="button" class="slick-next slick-arrow"><img src="icons/chevron-right.png"/></button>',
     responsive: [
       {
         breakpoint: 992,
