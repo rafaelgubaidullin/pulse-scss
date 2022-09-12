6cd7f90fe607b4bea04fcd90066ef82859e34ef3
Rafael <rafdigital22@gmail.com>
2022-09-12 09:52:46 +0300

    add tabs
    add code in JS & html

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -18,4 +18,34 @@ $(document).ready(function () {
       },
     ],
   });
+  $("ul.catalog__tabs").on(
+    "click",
+    "li:not(.catalog__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalog__tab_active")
+        .siblings()
+        .removeClass("catalog__tab_active")
+        .closest("div.container")
+        .find("div.catalog__cards")
+        .removeClass("catalog__cards_active")
+        .eq($(this).index())
+        .addClass("catalog__cards_active");
+    }
+  );
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalog-card__content")
+          .eq(i)
+          .toggleClass("catalog-card__content_active");
+        $(".catalog-card__list").eq(i).toggleClass("catalog-card__list_active");
+      });
+    });
+  }
+
+  toggleSlide(".catalog-card__link");
+  toggleSlide(".catalog-card__back");
 });
